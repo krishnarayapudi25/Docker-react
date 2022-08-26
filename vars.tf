61a3355c62dae57cd5653585b1a8dc97d2299eac
krishnarayapudi25 <95125632+krishnarayapudi25@users.noreply.github.com>
2022-08-26 11:46:20 +0530

    Update vars.tf

--- vars.tf
+++ vars.tf
@@ -1,6 +1,6 @@
 variable "access_key" {
-  default = "AKIA5WWVOOSQ6YE3DEKJ"
+  default = "##"
 }
 variable "secret_key" {
-    default = "H1r07zQBpr6JZJEMX/8Au0dEI5BwNa5sHRYgXx00"
+    default = "##"
 }
